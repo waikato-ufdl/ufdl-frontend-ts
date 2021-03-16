722b9f373432a8a29661d4931ed8dad3dd09d05d
csterling <coreytsterling@gmail.com>
2021-03-17 10:20:15 +1300

    Commented forEachOwnProperty.

--- src/util/typescript/forEachOwnProperty.ts
+++ src/util/typescript/forEachOwnProperty.ts
@@ -1,3 +1,11 @@
+/**
+ * Iterates over the owned properties of an object.
+ *
+ * @param obj
+ *          The object to iterate.
+ * @param body
+ *          The function to call for each property.
+ */
 export default function forEachOwnProperty<T extends {}>(
     obj: T,
     body: <P extends keyof T>(property: P, value: T[P]) => void
