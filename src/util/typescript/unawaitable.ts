c08d185741cfb4b044431924a6811d420391ad43
csterling <coreytsterling@gmail.com>
2021-03-17 10:01:18 +1300

    Refactored unawaitable.

--- src/util/typescript/unawaitable.ts
+++ src/util/typescript/unawaitable.ts
@@ -1,15 +1,18 @@
 import {discard} from "../discard";
 
 /**
- * Ensures that if an async routine is awaited, that it returns immediately.
+ * Ensures that an async routine cannot be awaited (returns immediately
+ * if await is used).
  *
  * @param body
  *          The async routine.
  */
-export function unawaitable(
-    body: () => Promise<void>
-): () => Promise<void> {
-    return async () => {
-        discard(body());
-    }
+export function unawaitable<B extends (...args: any) => Promise<void>>(
+    body: B
+): B {
+    return (
+        async (...args: any) => {
+            discard(body(...args));
+        }
+    ) as B;
 }
