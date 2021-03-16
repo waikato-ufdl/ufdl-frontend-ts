082a6881bf8d930b601adafb87149049e6681c34
csterling <coreytsterling@gmail.com>
2021-03-17 10:06:56 +1300

    Commented initialisers module.

--- src/util/typescript/initialisers.ts
+++ src/util/typescript/initialisers.ts
@@ -1,7 +1,27 @@
-export function constantInitialiser<T>(value: T): () => T {
+/**
+ * Creates an initialiser function which returns a constant value.
+ *
+ * @param value
+ *          The constant value the initialiser should return.
+ * @return
+ *          The initialiser function.
+ */
+export function constantInitialiser<T>(
+    value: T
+): () => T {
     return () => value
 }
 
+/**
+ * Creates an initialiser which returns a new instance of a class.
+ *
+ * @param cls
+ *          The class to initialise with.
+ * @param args
+ *          Any arguments to the class' constructor.
+ * @return
+ *          The initialiser function.
+ */
 export function classInitialiser<P extends readonly any[], T>(
     cls: {new(...args: P): T},
     ...args: P
