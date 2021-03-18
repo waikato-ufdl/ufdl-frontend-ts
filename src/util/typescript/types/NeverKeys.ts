8d7db47ff52947a37dfadc5abbd6db46d00e8fd7
csterling <coreytsterling@gmail.com>
2021-03-18 14:14:40 +1300

    Refactored/commented NeverKeys.

--- src/util/typescript/types/NeverKeys.ts
+++ src/util/typescript/types/NeverKeys.ts
@@ -1,4 +1,5 @@
-export type NeverKeys<T> = Exclude<
+/** Gets the declared key of a type T that map to the never type. */
+export type NeverKeys<T extends {}> = Exclude<
     { [K in keyof T]: Required<T>[K] extends never ? K : false }[keyof T],
     false
 >
