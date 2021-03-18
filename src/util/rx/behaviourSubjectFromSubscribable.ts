860f6d8c3a509eb871c6b62280fd140e8a49599f
csterling <coreytsterling@gmail.com>
2021-03-18 16:21:09 +1300

    Commented behaviourSubjectFromSubscribable.

--- src/util/rx/behaviourSubjectFromSubscribable.ts
+++ src/util/rx/behaviourSubjectFromSubscribable.ts
@@ -1,11 +1,23 @@
 import {BehaviorSubject, Subscribable} from "rxjs";
 
+/**
+ * Creates a behaviour subject which follows the given subscribable.
+ *
+ * @param subscribable
+ *          The subscribable to follow.
+ * @param initial
+ *          The initial value of the behaviour subject.
+ * @return
+ *          The behaviour subject.
+ */
 export default function behaviourSubjectFromSubscribable<T>(
     subscribable: Subscribable<T>,
     initial: T
 ) {
+    // Create the subject
     const subject = new BehaviorSubject(initial);
 
+    // Subscribe it to the subscribable
     subscribable.subscribe(subject);
 
     return subject;
