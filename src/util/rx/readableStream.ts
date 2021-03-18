1e3f49336c24d8a5b45fdd0fd324339b4033c6b2
csterling <coreytsterling@gmail.com>
2021-03-18 16:42:27 +1300

    Refactored/commented observeReadableStream.

--- src/util/rx/readableStream.ts
+++ src/util/rx/readableStream.ts
@@ -19,7 +19,18 @@ export function observableReadableStream<T>(
     )
 }
 
-async function observeReadableStream<T>(
+/**
+ * Calls the lifecycle methods of the observer based on information
+ * coming from a readable stream.
+ *
+ * @param stream
+ *          The stream to observe.
+ * @param observer
+ *          The observer.
+ * @return
+ *          A promise of completion of the stream.
+ */
+export async function observeReadableStream<T>(
     stream: ReadableStream<T>,
     observer: PartialObserver<T>
 ): Promise<void> {
