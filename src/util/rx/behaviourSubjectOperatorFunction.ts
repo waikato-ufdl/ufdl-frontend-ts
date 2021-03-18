e6d4e8eca7633bf56ccb0ec119a795a479a7b2a2
csterling <coreytsterling@gmail.com>
2021-03-18 16:24:16 +1300

    Commented behaviourSubjectOperatorFunction.

--- src/util/rx/behaviourSubjectOperatorFunction.ts
+++ src/util/rx/behaviourSubjectOperatorFunction.ts
@@ -1,13 +1,22 @@
 import {BehaviorSubject, OperatorFunction, UnaryFunction} from "rxjs";
 import behaviourSubjectFromSubscribable from "./behaviourSubjectFromSubscribable";
 
+/** The type of an operator function on behaviour subjects. */
 export interface BehaviourSubjectOperatorFunction<T, R>
     extends UnaryFunction<BehaviorSubject<T>, BehaviorSubject<R>> {}
 
+/**
+ * Converts an operator function into one that operates on behaviour subjects.
+ *
+ * @param operatorFunction
+ *          The operator function.
+ */
 export default function behaviourSubjectOperatorFunction<T, R>(
-    func: OperatorFunction<T, R>
+    operatorFunction: OperatorFunction<T, R>
 ): BehaviourSubjectOperatorFunction<T, R> {
     // Because the source is a behaviour subject, the initial value will be overwritten
     // immediately, so fudge it with null
-    return (source) => behaviourSubjectFromSubscribable(func(source), null) as any;
+    return (source) => {
+        return behaviourSubjectFromSubscribable(operatorFunction(source), null) as any;
+    }
 }
