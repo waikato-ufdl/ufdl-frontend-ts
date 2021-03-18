ee0b4eb9b8e299a4e732a02030107b85979e26b7
csterling <coreytsterling@gmail.com>
2021-03-18 16:53:42 +1300

    Refactored/commented dataStream.ts.

--- src/util/rx/dataStream.ts
+++ src/util/rx/dataStream.ts
@@ -5,21 +5,40 @@ import behaviourSubjectOperatorFunction from "./behaviourSubjectOperatorFunction
 import {scan} from "rxjs/operators";
 import behaviourSubjectFromSubscribable from "./behaviourSubjectFromSubscribable";
 
+/*
+ * Functionality for using a progressive download of data in RX.
+ */
+
+/** A readable stream of data. */
 export type DataStream = ReadableStream<Uint8Array>
 
+/** A download progression of data. */
 export type DataStreamSubject = BehaviorSubject<Uint8Array>
 
+/**
+ * Progressively buffers data from a data stream as a behaviour subject.
+ *
+ * @param dataStream
+ *          The data stream to buffer.
+ * @return
+ *          A behaviour subject representing the amount of data downloaded so far.
+ */
 export function dataStreamSubject(
     dataStream: DataStream
 ): DataStreamSubject {
-    return behaviourSubjectOperatorFunction(
+    // Create an operator function that accumulates data for a behaviour subject
+    const dataAccumulator = behaviourSubjectOperatorFunction(
         scan(
             (acc: Uint8Array, value: Uint8Array) => concatUint8Array(acc, value)
         )
-    )(
-        behaviourSubjectFromSubscribable(
-            observableReadableStream(dataStream),
-            new Uint8Array()
-        )
-    )
+    );
+
+    // Create a behaviour subject that observes the data stream
+    const dataSubject = behaviourSubjectFromSubscribable(
+        observableReadableStream(dataStream),
+        new Uint8Array()
+    );
+
+    // Make the subject accumulate (buffer) the data
+    return dataAccumulator(dataSubject);
 }
