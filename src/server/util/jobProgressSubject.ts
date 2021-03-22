0207cdcc12b7352119e7cae66b4fd600e4d1ef3e
csterling <coreytsterling@gmail.com>
2021-03-23 11:03:32 +1300

    Commented jobProgressSubject.

--- src/server/util/jobProgressSubject.ts
+++ src/server/util/jobProgressSubject.ts
@@ -2,16 +2,35 @@ import {BehaviorSubject, Observable} from "rxjs";
 import {RawJSONObject} from "ufdl-js-client/types";
 import {filter, map} from "rxjs/operators";
 
+/**
+ * Creates a behaviour subject which tracks the progress of a job.
+ *
+ * @param observable
+ *          The observable web-socket connected to the job.
+ * @return
+ *          A behaviour subject tracking the progress value.
+ */
 export default function jobProgressSubject(
     observable: Observable<RawJSONObject>
 ): BehaviorSubject<number> {
-
+    // Create a behaviour subject starting at 0%
     const subject = new BehaviorSubject<number>(0.0);
 
-    const filterProgressUpdates = filter((value: RawJSONObject) => 'progress' in value && typeof value['progress'] === 'number');
+    // Create an operator which filters to only updates containing a value for the progress amount
+    const filterProgressUpdates = filter(
+        (value: RawJSONObject) => {
+            return 'progress' in value && typeof value['progress'] === 'number'
+        }
+    );
 
-    const extractProgress = map((value: RawJSONObject) => value['progress'] as number);
+    // Create an operator which maps the JSON message to the progress amount
+    const extractProgress = map(
+        (value: RawJSONObject) => {
+            return value['progress'] as number
+        }
+    );
 
+    // Subscribe the subject to the filtered, mapped version of the web-socket
     extractProgress(filterProgressUpdates(observable)).subscribe(subject);
     
     return subject;
