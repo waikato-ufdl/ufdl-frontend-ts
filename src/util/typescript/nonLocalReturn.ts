e0e29851cad03e0d5dbf72e8d30736bbe2a99938
csterling <coreytsterling@gmail.com>
2021-03-25 15:47:21 +1300

    Refactor non-local returns for use with async.

--- src/util/typescript/nonLocalReturn.ts
+++ src/util/typescript/nonLocalReturn.ts
@@ -1,8 +1,31 @@
+import isPromise from "./async/isPromise";
+
 /**
  * Helper class which carries the value of a non-local return call.
  */
 class NonLocalReturnException<T> {
     constructor(readonly value: T) {}
+}
+
+/**
+ * The type of the function called to enact a non-local return.
+ */
+type NonLocalReturnFunction<T> = void extends T ? (() => never) : ((value: T) => never)
+
+/**
+ * Handles the catch block for a non-local return exception.
+ *
+ * @param e
+ *          The exception.
+ */
+function handleCatchNonLocalReturn<T>(
+    e: any
+): T {
+    // If a non-local return was executed, return the value
+    if (e instanceof NonLocalReturnException) return e.value;
+
+    // Rethrow on any other exception
+    throw e;
 }
 
 /**
@@ -15,23 +38,24 @@ class NonLocalReturnException<T> {
  *          The value passed to the non-local return call, or the result
  *          of the function if it completes normally.
  */
-export function withNonLocalReturn<T>(
-    body: (nonLocalReturn: (value: T) => never) => T
+export function withNonLocalReturn<T = void>(
+    body: (nonLocalReturn: NonLocalReturnFunction<T>) => T
 ): T {
     // Define the non-local return function
-    function nonLocalReturn(value: T): never {
-        throw new NonLocalReturnException(value);
-    }
+    const nonLocalReturn = (
+        (value) => {throw new NonLocalReturnException(value)}
+    ) as NonLocalReturnFunction<T>;
 
     try {
         // Execute the body of the function with the non-local return context
-        return body(nonLocalReturn);
+        let result = body(nonLocalReturn);
+
+        // If the body is asynchronous, attach a catch handler
+        if (isPromise(result)) result = result.catch(handleCatchNonLocalReturn) as any;
+
+        return result;
 
     } catch (e) {
-        // If a non-local return was executed, return the value
-        if (e instanceof NonLocalReturnException) return e.value;
-
-        // Rethrow on any other exception
-        throw e;
+        return handleCatchNonLocalReturn<T>(e);
     }
 }
