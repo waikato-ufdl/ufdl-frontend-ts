d8c742be915e543fceb03ce342b0976b25ad593a
csterling <coreytsterling@gmail.com>
2021-03-26 15:27:06 +1300

    Removed unnecessary callOrPass function.

--- src/util/typescript/callOrPass.ts
+++
@@ -1,6 +0,0 @@
-export default function callOrPass<F extends (...args: P) => R, P extends any[], R>(
-    f: F | undefined
-): (...args: P) => (R | undefined) {
-    if (f === undefined) return () => undefined;
-    return f;
-}
