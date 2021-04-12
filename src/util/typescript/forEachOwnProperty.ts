c4778b9e3e85ffb73cb9c30154cfd418360477f4
csterling <coreytsterling@gmail.com>
2021-04-12 12:27:36 +1200

    Removed incorrect generic typing.

--- src/util/typescript/forEachOwnProperty.ts
+++ src/util/typescript/forEachOwnProperty.ts
@@ -8,7 +8,7 @@
  */
 export default function forEachOwnProperty<T extends {}>(
     obj: T,
-    body: <P extends keyof T>(property: P, value: T[P]) => void
+    body: (property: keyof T, value: T[keyof T]) => void
 ): void {
     for (const property in obj) {
         if (!obj.hasOwnProperty(property)) continue;
