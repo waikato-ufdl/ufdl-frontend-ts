6bd8719a89a52bd2a781387a2ff2782233a018a5
csterling <coreytsterling@gmail.com>
2021-04-14 11:55:49 +1200

    Fixed bug where completed behaviour subjects were not transferring final value through operator.

--- src/util/rx/behaviourSubjectOperatorFunction.ts
+++ src/util/rx/behaviourSubjectOperatorFunction.ts
@@ -1,5 +1,6 @@
-import {BehaviorSubject, OperatorFunction, UnaryFunction} from "rxjs";
+import {BehaviorSubject, of, OperatorFunction, UnaryFunction} from "rxjs";
 import behaviourSubjectFromSubscribable from "./behaviourSubjectFromSubscribable";
+import {concatAll} from "rxjs/operators";
 
 /** The type of an operator function on behaviour subjects. */
 export interface BehaviourSubjectOperatorFunction<T, R>
@@ -17,6 +18,13 @@ export default function behaviourSubjectOperatorFunction<T, R>(
     // Because the source is a behaviour subject, the initial value will be overwritten
     // immediately, so fudge it with null
     return (source) => {
-        return behaviourSubjectFromSubscribable(operatorFunction(source), null) as any;
+
+        const initial = operatorFunction(of(source.value));
+
+        const onGoing = operatorFunction(source);
+
+        const concatted = concatAll<R>()(of(initial, onGoing));
+
+        return behaviourSubjectFromSubscribable(concatted, null as any);
     }
 }
