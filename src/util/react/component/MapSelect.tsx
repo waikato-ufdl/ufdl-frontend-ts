98769864e8e9337be071adb231f1606e4beb81c5
csterling <coreytsterling@gmail.com>
2021-04-15 15:12:36 +1200

    Commented MapSelect.

--- src/util/react/component/MapSelect.tsx
+++ src/util/react/component/MapSelect.tsx
@@ -7,53 +7,80 @@ import {mapToArray} from "../../map";
 
 
 export type MapSelectProps<V> = {
+    /** The map from option labels to their corresponding value. */
     values: ReadonlyMap<string, V>
+
+    /** The controlled value of the drop-down (by label), or a non-controlling flag. */
     value: Controllable<string | undefined>
+
+    /** The action to perform when the selected value changes (default: do nothing). */
     onChange?: (value: Possible<V>, key?: string) => void
+
+    /** Whether the drop-down should be disabled (default: false). */
     disabled?: boolean
 }
 
+/**
+ * A select drop-down which lets the user select a value (of
+ * any type V) by its corresponding string label. The relationship between
+ * value and label is provided in the form of a map from label to value.
+ *
+ * Includes an option for no selection.
+ *
+ * @param props
+ *          The properties of the drop-down.
+ */
 export function MapSelect<V>(
     props: MapSelectProps<V>
 ) {
-    // Create state
-    const [selected, setSelected, selectedLocked] = useControllableState<string | undefined>(
-        props.value,
-        constantInitialiser(undefined)
-    );
-
-    // Extract props
+    // Destructure the props
     const {
         values,
+        value,
         onChange,
         disabled
     } = props;
 
-    // Create an option for each value
+    // Create controllable state for the selected value
+    const [selectedLabel, setSelectedLabel] = useControllableState<string | undefined>(
+        value,
+        constantInitialiser(undefined)
+    );
+
+    // Create an option for each selectable value
     const valueOptions = mapToArray(
         values,
         (key) => <option key={key} value={`+${key}`}>{key}</option>
     );
 
     // Create the absent option
-    const absentOption = <option value={""}>{""}</option>;
+    const absentOption = <option key={""} value={""}>{""}</option>;
 
+    // Create an event handler which maps the disambiguation key to label/value
     const onChangeActual = asChangeEventHandler(
-        (value) => {
-            const key = value === "" ? undefined : value.slice(1);
+        (key) => {
+            // Undo the disambiguation formatting done when creating the option
+            const label = key === ""
+                ? undefined
+                : key.slice(1);
 
-            setSelected(key);
+            // Attempt to change the selected label state
+            setSelectedLabel(label);
 
-            const actualValue = key === undefined ? Absent : values.get(key);
+            // Get the corresponding value for the label (Absent for the no-selection state)
+            const value = label === undefined
+                ? Absent
+                : values.get(label)!;
 
-            if (onChange !== undefined) onChange(actualValue!, key);
+            // Call the change handler if one was provided
+            if (onChange !== undefined) onChange(value, label);
         }
     );
 
     return <select
         className={"MapSelect"}
         onChange={onChangeActual}
-        value={selected === undefined ? "" : `+${selected}`}
+        value={selectedLabel === undefined ? "" : `+${selectedLabel}`}
         disabled={disabled}
     >
         {absentOption}
