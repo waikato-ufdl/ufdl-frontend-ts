6b266da759b26d3ca13c4359d549b9de88b17d21
csterling <coreytsterling@gmail.com>
2021-04-16 14:44:45 +1200

    Made all error values differentiable.

--- src/server/websocket/observableWebSocket.ts
+++ src/server/websocket/observableWebSocket.ts
@@ -1,18 +1,22 @@
 import {Observable} from "rxjs";
-import {RawJSONObject} from "ufdl-ts-client/types/raw";
 import UFDLServerContext from "ufdl-ts-client/UFDLServerContext";
 import * as Job from "ufdl-ts-client/functional/core/jobs/job";
-import {JobTransitionHandlers} from "ufdl-ts-client/types/core/jobs/job";
+import {JobTransitionHandlers, JobTransitionMessage} from "ufdl-ts-client/types/core/jobs/job";
 
 export const CANCELLED = Symbol("The job was cancelled");
+export const WEB_SOCKET_CLOSED_UNEXPECTEDLY = Symbol("The web-socket closed without warning");
+
+export class WebSocketError {
+    constructor(public readonly event: Event) {}
+}
 
 export default function observableWebSocket(
     context: UFDLServerContext,
     jobPK: number,
     errorOnError: boolean = false,
     errorOnCancel: boolean = false
-) {
-    return new Observable<RawJSONObject>(
+): Observable<JobTransitionMessage> {
+    return new Observable<JobTransitionMessage>(
         (subscriber) => {
             const handlers: JobTransitionHandlers = {
                 acquire(json) {subscriber.next(json);},
@@ -42,12 +46,12 @@ export default function observableWebSocket(
                 handlers,
                 (self) => {
                     if (!self)
-                        subscriber.error({error: 'web-socket closed unexpectedly'});
+                        subscriber.error(WEB_SOCKET_CLOSED_UNEXPECTEDLY);
                     else if (!subscriber.closed)
                         subscriber.complete();
 
                 },
-                (event) => console.error(`Web-socket connected to job #${jobPK} encountered an error:`, event)
+                (event) => subscriber.error(new WebSocketError(event))
             );
         }
     )
