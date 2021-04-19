955bf6d8904049a5f9199d02ef66b71afc6dfdf6
csterling <coreytsterling@gmail.com>
2021-04-20 09:05:45 +1200

    Commented isBehaviourSubject.

--- src/util/rx/isBehaviourSubject.ts
+++ src/util/rx/isBehaviourSubject.ts
@@ -1,5 +1,13 @@
 import {BehaviorSubject} from "rxjs";
 
+/**
+ * Determines if the provided argument is a behaviour subject.
+ *
+ * @param value
+ *          The value to check.
+ * @return
+ *          true if the argument is a behaviour subject, false if not.
+ */
 export default function isBehaviourSubject(
     value: any
 ): value is BehaviorSubject<any> {
