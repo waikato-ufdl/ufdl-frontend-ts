ed68fecb67f76bc7d7c8685f1efb2647c79a1622
csterling <coreytsterling@gmail.com>
2021-04-28 15:51:23 +1200

    Fixed compose.

--- src/util/typescript/functions/compose.ts
+++ src/util/typescript/functions/compose.ts
@@ -1,10 +1,32 @@
-export default function compose<P extends readonly unknown[], R extends readonly unknown[], R2>(a: (...args: P) => R, b: (...args: R) => R2): R2;
-export default function compose<P extends readonly unknown[], R, R2>(a: (...args: P) => R, b: (arg: R) => R2): R2;
+import {isArray} from "../arrays/isArray";
+
+export default function compose<
+    P extends readonly unknown[],
+    R extends readonly unknown[],
+    R2
+>(
+    a: (...args: P) => R,
+    b: (...args: R) => R2
+): (...args: P) => R2;
+
+export default function compose<
+    P extends readonly unknown[],
+    R,
+    R2
+>(
+    a: (...args: P) => R,
+    b: (arg: R) => R2
+): (...args: P) => R2;
+
 export default function compose(
     a: (...args: readonly unknown[]) => unknown,
     b: (...args: readonly unknown[]) => unknown
 ): (...args: readonly unknown[]) => unknown {
     return (...args) => {
-        return b(a(...args));
+        const intermediate = a(...args);
+        if (isArray(intermediate))
+            return b(...args);
+        else
+            return b(args);
     }
 }
