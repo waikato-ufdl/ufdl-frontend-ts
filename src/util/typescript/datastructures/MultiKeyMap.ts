0ece56f50cac88b9ea9b43ec8a379a69eae5f500
csterling <coreytsterling@gmail.com>
2021-04-28 17:02:42 +1200

    Refactored multi-key map to remove need for boxing of values.

--- src/util/typescript/datastructures/MultiKeyMap.ts
+++ src/util/typescript/datastructures/MultiKeyMap.ts
@@ -1,74 +1,71 @@
 import {Head} from "../types/array/Head";
 import {Tail} from "../types/array/Tail";
-import {mapAll, mapReduce} from "../../map";
+import {mapGetDefault, mapReduce} from "../../map";
 import {Absent, absentAsUndefined, isPresent, Possible} from "../types/Possible";
+import {constantInitialiser} from "../initialisers";
+
 
 export class MultiKeyMap<K extends readonly any[], V>
     // implements Map<K, V>
 {
-    private value: Possible<V> = Absent;
+    /** Stores the values for zero-length keys. */
+    private static TOP_LEVEL_VALUES: Map<MultiKeyMap<any, any>, any> = new Map();
 
-    private subMap: Map<Head<K>, MultiKeyMap<Tail<K>, V>> | undefined = undefined;
+    /** Map from the head of the key to values for those heads. */
+    private valueMap: Map<Head<K>, V> | undefined = undefined;
+
+    /** Map from the head of the key to child maps for the key remainder. */
+    private childMap: Map<Head<K>, MultiKeyMap<Tail<K>, V>> | undefined = undefined;
 
     readonly [Symbol.toStringTag]: string = "MultiKeyMap";
 
     get size(): number {
-        return (isPresent(this.value) ? 1 : 0) + (
-            this.subMap === undefined ?
-                0 :
-                mapReduce(
-                    this.subMap,
-                    0,
-                    (current, _, value) =>
-                        current + value.size
-                )
-        )
+        const thisLevelSize = this.valueMap !== undefined ? this.valueMap.size : 0;
+        const topLevelSize = MultiKeyMap.TOP_LEVEL_VALUES.has(this) ? 1 : 0;
+        const childrenSize = this.childMap === undefined
+            ? 0
+            : mapReduce(
+                this.childMap,
+                0,
+                (current, _, value) =>
+                    current + value.size
+            );
+        return thisLevelSize + topLevelSize + childrenSize;
     }
 
     get empty(): boolean {
-        if (isPresent(this.value))
-            return false;
-        else if (this.subMap === undefined)
-            return true;
-        else if (this.subMap.size === 0)
-            return true;
-        else
-            return mapAll(
-                this.subMap,
-                (
-                    _,
-                    subMap
-                ) => {
-                    return subMap.empty;
-                }
-            );
+        return this.size === 0;
     }
 
     clear(): void {
-        this.value = Absent;
-        this.subMap = undefined;
+        MultiKeyMap.TOP_LEVEL_VALUES.delete(this);
+        this.valueMap = undefined;
+        this.childMap = undefined;
     }
 
     delete(key: Readonly<K>): boolean {
         if (key.length === 0) {
-            const present: boolean = isPresent(this.value);
-            this.value = Absent;
-            return present;
+            return MultiKeyMap.TOP_LEVEL_VALUES.delete(this);
         }
 
-        if (this.subMap === undefined) return false;
+        if (key.length === 1) {
+            if (this.valueMap === undefined) return false;
+            return this.valueMap.delete(key[0]);
+        }
+
+        if (this.childMap === undefined) return false;
 
         const [head, ...tail] = key;
 
-        const subMapAtHead = this.subMap.get(head);
+        const childMapForHead = this.childMap.get(head);
 
-        if (subMapAtHead === undefined) return false;
+        if (childMapForHead === undefined) return false;
 
-        const deleted = subMapAtHead.delete(tail as Tail<K>);
+        const deleted = childMapForHead.delete(tail as Tail<K>);
 
         if (deleted) {
-            if (subMapAtHead.size === 0) this.subMap.delete(head);
-            if (this.subMap.size === 0) this.subMap = undefined;
+            if (childMapForHead.size === 0) this.childMap.delete(head);
+            if (this.childMap.size === 0) this.childMap = undefined;
         }
 
         return deleted;
@@ -76,18 +73,23 @@ export class MultiKeyMap<K extends readonly any[], V>
 
     private getInternal(key: Readonly<K>): Possible<V> {
         if (key.length === 0) {
-            return this.value;
+            return mapGetDefault(MultiKeyMap.TOP_LEVEL_VALUES, this, constantInitialiser(Absent), false);
         }
 
-        if (this.subMap === undefined) return Absent;
+        if (key.length === 1) {
+            if (this.valueMap === undefined) return Absent;
+            return mapGetDefault(this.valueMap as ReadonlyMap<Head<K>, V | typeof Absent>, key[0], constantInitialiser(Absent), false);
+        }
+
+        if (this.childMap === undefined) return Absent;
 
         const [head, ...tail] = key;
 
-        const subMapAtHead = this.subMap.get(head);
+        const childMapForHead = this.childMap.get(head);
 
-        if (subMapAtHead === undefined) return Absent;
+        if (childMapForHead === undefined) return Absent;
 
-        return subMapAtHead.getInternal(tail as Tail<K>);
+        return childMapForHead.getInternal(tail as Tail<K>);
     }
 
     get(key: Readonly<K>): V | undefined {
@@ -101,19 +103,25 @@ export class MultiKeyMap<K extends readonly any[], V>
 
     set(key: Readonly<K>, value: V): this {
         if (key.length === 0) {
-            this.value = value;
+            MultiKeyMap.TOP_LEVEL_VALUES.set(this, value);
             return this;
         }
 
-        if (this.subMap === undefined) this.subMap = new Map();
+        if (key.length === 1) {
+            if (this.valueMap === undefined) this.valueMap = new Map();
+            this.valueMap.set(key[0], value);
+            return this;
+        }
+
+        if (this.childMap === undefined) this.childMap = new Map();
 
         const [head, ...tail] = key;
 
-        let subMapAtHead = this.subMap.get(head);
+        let subMapAtHead = this.childMap.get(head);
 
         if (subMapAtHead === undefined) {
             subMapAtHead = new MultiKeyMap();
-            this.subMap.set(head, subMapAtHead);
+            this.childMap.set(head, subMapAtHead);
         }
 
         subMapAtHead.set(tail as Tail<K>, value);
