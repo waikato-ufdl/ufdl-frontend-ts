e9ec4479e2601ef3e710807c8c5bac370110e9e2
csterling <coreytsterling@gmail.com>
2021-05-05 09:07:55 +1200

    Added a function which returns the result of a promise as a Result type.

---
+++ src/util/typescript/async/promiseAsResult.ts
@@ -0,0 +1,26 @@
+import {Result} from "../types/Result";
+
+/**
+ * Returns a promise that never fails, but instead returns
+ * a [Result] type with the value/error.
+ *
+ * @param promise
+ *          The promise to convert.
+ * @return
+ *          A promise of an equivalent result type.
+ */
+export default async function promiseAsResult<T>(
+    promise: Promise<T>
+): Promise<Result<T>> {
+    try {
+        return {
+            success: true,
+            value: await promise
+        }
+    } catch (error: any) {
+        return {
+            success: false,
+            error
+        }
+    }
+}
