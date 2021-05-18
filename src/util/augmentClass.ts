96ecf62b9533158263e9f5e255ccd9bf37150059
csterling <coreytsterling@gmail.com>
2021-05-18 14:49:05 +1200

    Commented augmentClass.ts.

--- src/util/augmentClass.ts
+++ src/util/augmentClass.ts
@@ -1,9 +1,20 @@
 import {HTMLAttributes} from "react";
 
+/**
+ * Augments the props object of a component with additional class-names.
+ *
+ * @param props
+ *          The props object to augment.
+ * @param classNames
+ *          The additional classes to add.
+ * @return
+ *          An equivalent props object with the additional classes.
+ */
 export function augmentClass<P extends HTMLAttributes<any>>(
     props: P,
     ...classNames: string[]
 ): P {
+    // If no class-names provided, no augmentation is necessary
     if (classNames.length === 0) return props;
 
     return {
@@ -12,15 +23,30 @@ export function augmentClass<P extends HTMLAttributes<any>>(
     }
 }
 
+/**
+ * Augments the className property of a props object with
+ * the given additional class-names.
+ *
+ * @param className
+ *          The current value of the className property.
+ * @param classNames
+ *          The additional classes to add.
+ * @return
+ *          The augmented value of the className property.
+ */
 export function augmentClassName(
     className: string | undefined,
     ...classNames: string[]
 ): string | undefined {
+    // If no additional class-names were provided, className remains unchanged
     if (classNames.length === 0) return className;
 
+    // Join the provided class-names with a space
     const joined = classNames.join(" ");
 
+    // If the current value is undefined, the final value is just the joined addenda
     if (className === undefined) return joined;
 
+    // Otherwise it's the current value joined with the addenda
     return className + " " + joined;
 }
