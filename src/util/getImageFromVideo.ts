fac3abc395bac32071b6b3a2d6fd41698aece043
csterling <coreytsterling@gmail.com>
2021-11-25 15:29:52 +1300

    Added function to extract the current frame of a video as an image.

---
+++ src/util/getImageFromVideo.ts
@@ -0,0 +1,54 @@
+import {rendezvous} from "./typescript/async/rendezvous";
+
+/**
+ * Extracts the current frame as an image from the given
+ * video in the requested format.
+ *
+ * @param video
+ *          The video element to extract the current frame from.
+ * @param type
+ *          The format to extract the image in (png/jpeg).
+ * @param width
+ *          The width to scale the image to. Defaults to the video's native width.
+ * @param height
+ *          The height to scale the image to. Defaults to the video's native height.
+ */
+export default async function getImageFromVideo(
+    video: HTMLVideoElement,
+    type: "png" | "jpeg" = "png",
+    width: number = video.videoWidth,
+    height: number = video.videoHeight
+): Promise<Blob> {
+    // Create a canvas of the desired width and height
+    const dummyCanvas = document.createElement("canvas")
+    dummyCanvas.width = width
+    dummyCanvas.height = height
+
+    // Get the 2D drawing context for drawing the video into
+    const context = dummyCanvas.getContext("2d")
+    if (context === null) {
+        // Should never occur
+        throw new Error("getContext returned null; this should never occur")
+    }
+
+    // Draw the video into the canvas, scaling to the desired dimensions
+    context.drawImage(video, 0, 0, width, height)
+
+    // The extracted image data is provided through a callback, so create a rendezvous
+    // promise to receive the value
+    const [promise, resolve, reject] = rendezvous<Blob>();
+
+    // Schedule the promise to be resolved with the image data in the requested format
+    dummyCanvas.toBlob(
+        (blob) => {
+            if (blob === null) {
+                reject("toBlob returned null");
+            } else {
+                resolve(blob)
+            }
+        },
+        `image/${type}`
+    )
+
+    return promise;
+}
