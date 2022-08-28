492986b95f9e6cdd8cabaa36fb84e6c3f7973cb8
csterling <coreytsterling@gmail.com>
2022-08-29 08:55:09 +1200

    Removed delay from objdet annotation submission.

--- src/server/hooks/useObjectDetectionDataset/ObjectDetectionDatasetDispatch.ts
+++ src/server/hooks/useObjectDetectionDataset/ObjectDetectionDatasetDispatch.ts
@@ -1,14 +1,11 @@
 import {MutableDatasetDispatch, MutableDatasetDispatchItem} from "../useDataset/DatasetDispatch";
-import {NO_ANNOTATION, OptionalAnnotations} from "../../types/annotations";
+import {NO_ANNOTATION} from "../../types/annotations";
 import {IAnnotation} from "react-picture-annotation/dist/types/src/Annotation";
 import {IRectShapeData} from "react-picture-annotation/dist/types/src/Shape";
 import {DomainAnnotationType, DomainDataType} from "../../domains";
 import isDefined from "../../../util/typescript/isDefined";
 import hasData from "../../../util/react/query/hasData";
-import delayFunction from "../../../util/typescript/delayFunction";
-import {mapGetDefault} from "../../../util/map";
 import {detectedObjectsToIAnnotations} from "../../../util/IAnnotations";
-import {isDeepEqual} from "../../../util/equivalency";
 
 export class ObjectDetectionDatasetDispatchItem
     extends MutableDatasetDispatchItem<
@@ -35,30 +32,6 @@ export default class ObjectDetectionDatasetDispatch
         DomainAnnotationType<'Object Detection'>,
         ObjectDetectionDatasetDispatchItem
 > {
-    // Create a delayed version of the annotation change function which doesn't
-    // update the server until the user has stopped making changes for at least
-    // half a second
-    private delayedSetAnnotationsForFile: Map<string, (annotations: OptionalAnnotations<DomainAnnotationType<"Object Detection">>) => void>
-        = new Map()
-
-    setAnnotationsForFile(filename: string, annotations: OptionalAnnotations<DomainAnnotationType<"Object Detection">>) {
-        // Make sure the annotations have changed before sending
-        const currentAnnotations = this.get(filename)?.annotations?.data as OptionalAnnotations<DomainAnnotationType<"Object Detection">>
-        if (annotations === NO_ANNOTATION && currentAnnotations === NO_ANNOTATION) return
-        if (annotations !== NO_ANNOTATION && currentAnnotations !== NO_ANNOTATION) {
-            if (isDeepEqual(annotations, currentAnnotations)) return
-        }
-
-        const delayedFunction = mapGetDefault(
-            this.delayedSetAnnotationsForFile,
-            filename,
-            () => delayFunction((annotations) => super.setAnnotationsForFile(filename, annotations), 500),
-            true
-        )
-
-        delayedFunction(annotations)
-    }
-
     public asIAnnotations(filename: string): IAnnotation<IRectShapeData>[] | Map<number, IAnnotation<IRectShapeData>[]> | typeof NO_ANNOTATION | undefined {
         const objects = this.get(filename);
 
