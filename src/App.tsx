b64b87c883235b73eddeca59242d51c75d25d939
csterling <coreytsterling@gmail.com>
2022-09-29 11:52:37 +1300

    Remove scheduler as it is now not necessary.

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,11 @@ import Window from "./components/Window";
 import UFDLServerContext from "ufdl-ts-client/UFDLServerContext";
 import {MainMenuPage} from "./components/pages/MainMenuPage";
 import {QueryClient, QueryClientProvider} from "react-query"
-import {NQueue} from "./server/NQueue";
 
 const queryClient = new QueryClient()
 
 export default function App() {
-    const context = UFDLServerContext.for_current_host("", "", localStorage, new NQueue(5));
+    const context = UFDLServerContext.for_current_host("", "", localStorage);
 
     return <div className="App fullScreen">
         <QueryClientProvider client={queryClient}>
