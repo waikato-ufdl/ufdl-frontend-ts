ac41cba69cceb2f511a58728c05a39ca6c215376
csterling <coreytsterling@gmail.com>
2022-10-07 12:37:39 +1300

    Commented oneShot.

--- src/util/typescript/functions/oneShot.ts
+++ src/util/typescript/functions/oneShot.ts
@@ -1,14 +1,23 @@
-import isDefined from "../isDefined";
-
+/**
+ * Makes a procedure idempotent. Calling the return function will
+ * call the wrapped function on first invocation, and do nothing
+ * on all subsequent invocations.
+ *
+ * @param func
+ *          The procedure to wrap.
+ * @return
+ *          A one-shot equivalent of the wrapped function.
+ */
 export default function oneShot<P extends  readonly unknown[]>(
     func: (...args: P) => void
 ): (...args: P) => void {
-    let closure: ((...args: P) => void) | undefined = func
+    //  Create state of what to call next, the function or nothing
+    let toCall: ((...args: P) => void) | null = func
 
     return (...args: P) => {
-        if (isDefined(closure)) {
-            closure(...args)
-            closure = undefined
+        if (toCall !== null) {
+            toCall(...args)
+            toCall = null
         }
     }
 }
