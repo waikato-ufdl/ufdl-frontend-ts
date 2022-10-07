f3a6cbaa7edce3476c8d17a5263abd8fd78e9238
csterling <coreytsterling@gmail.com>
2022-10-07 13:12:27 +1300

    Refined arrayMap typing.

--- src/util/typescript/arrays/arrayMap.ts
+++ src/util/typescript/arrays/arrayMap.ts
@@ -1,9 +1,12 @@
+import {RemoveReadonly} from "../types/RemoveReadonly";
+import {IndexType} from "../types/array/IndexType";
+
 export default function arrayMap<
     TArray extends readonly unknown[],
     TReturn
 >(
     array: TArray,
-    mapFn: (element: TArray[number], index: number) => TReturn
-): { [TKey in keyof TArray]: TReturn } {
+    mapFn: (element: TArray[number], index: IndexType<TArray>) => TReturn
+): RemoveReadonly<{ [TKey in keyof TArray]: TReturn }> {
     return array.map(mapFn as any) as any
 }
