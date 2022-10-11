d996b212754a1a17ac20bca63060d73d88fda88e
csterling <coreytsterling@gmail.com>
2022-10-11 14:45:24 +1300

    Added type for function components, and made function-component return-type generic.

--- src/util/react/types.ts
+++ src/util/react/types.ts
@@ -1,9 +1,16 @@
-import {ReactElement} from "react";
+import {JSXElementConstructor, ReactElement} from "react";
 
 /**
  * The return-type of a function component.
  */
-export type FunctionComponentReturnType = ReactElement<any, any> | null
+export type FunctionComponentReturnType<P = any, T extends string | JSXElementConstructor<any> = string | JSXElementConstructor<any>>
+    = ReactElement<P, T> | null
+
+/**
+ * The type of a function component.
+ */
+export type FunctionComponent<P = any, T extends string | JSXElementConstructor<any> = string | JSXElementConstructor<any>>
+    = (props: P) => FunctionComponentReturnType<P, T>
 
 /**
  * Either a single T, or an array of T.
