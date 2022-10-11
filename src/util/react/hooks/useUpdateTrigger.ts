61dd49013ec5e3bfb83107769055aa0fb7e084d5
csterling <coreytsterling@gmail.com>
2022-10-11 14:45:52 +1300

    Commented useUpdateTrigger.

--- src/util/react/hooks/useUpdateTrigger.ts
+++ src/util/react/hooks/useUpdateTrigger.ts
@@ -1,7 +1,14 @@
 import {useReducer} from "react";
 import {identity} from "../../identity";
 
+/**
+ * Hook which provides a callback which can be used to trigger a React
+ * render.
+ */
 export default function useUpdateTrigger(): () => void {
+    // A reducer which updates its state to a new empty array each time
+    // the dispatch is called. React sees the new array as new state, so
+    // triggers a render
     return useReducer(
         () => [],
         [],
