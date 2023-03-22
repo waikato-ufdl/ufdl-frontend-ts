b67405140ed5ebf56412884401272ea556999d15
Peter Reutemann <fracpete@gmail.com>
2023-03-23 09:14:53 +1300

    added 'new dataset' for now

--- src/components/pages/MainMenuPage.tsx
+++ src/components/pages/MainMenuPage.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import LoginPage from "./LoginPage";
 import MenuPage from "./MenuPage";
 import useStateSafe from "../../util/react/hooks/useStateSafe";
+import NewDatasetPage from "./NewDatasetPage";
 import NewTeamPage from "./NewTeamPage";
 import TheLoopPage from "./loop/TheLoopPage";
 import {UNCONTROLLED_KEEP} from "../../util/react/hooks/useControllableState";
@@ -22,10 +23,11 @@ export function MainMenuPage() {
         />
     }
 
-    return <MenuPage titleGenerator={["Create User", "Create Team", "Create Project", "Start", "Settings"]}>
+    return <MenuPage titleGenerator={["Create User", "Create Team", "Create Project", "Create Dataset", "Start", "Settings"]}>
         <NewUserPage />
         <NewTeamPage />
         <NewProjectPage teamPK={UNCONTROLLED_KEEP} />
+        <NewDatasetPage domain={UNCONTROLLED_KEEP} licencePK={UNCONTROLLED_KEEP} isPublic={UNCONTROLLED_KEEP}/>
         <TheLoopPage />
         <SettingsPage />
     </MenuPage>
